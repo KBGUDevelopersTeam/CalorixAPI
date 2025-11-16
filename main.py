dac67db32711cd33be11b72e20eb65c677beeae9
Danilka <danil25032005cod@gmal.com>
2025-11-16 12:18:05 +0300

    add fastapi to main.py file

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # место для написания основной логики API
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/User")
+def home() -> dict:
+    return {
+        "id":"1",
+        "email":"admin@admin.com",
+        "password":"Admin"
+    }
+
